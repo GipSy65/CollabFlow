751d89fb4ce585fcfaa5456379395b26a176ccc0
ashutos <tripathyashutosh65@gmail.com>
2025-02-10 17:28:42 +0530

    User API

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -8,9 +8,15 @@ const verifyFirebaseToken = async (req, res, next) => {
         return res.status(401).json({ message: "Unauthorized - No Token Provided"});
     }try{
         const decodedToken = await admin.auth().verifyIdToken(token);
+
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (decodedToken.exp < currentTime) {
+            return res.status(401).json({ message: "Token Expired"});
+        }
         req.user = decodedToken;
         next();
     }catch(err){
+        console.error("Token Verification error",err.message);
         return res.status(403).json({ message: "Invalid or Expired Token"});
     }
 };
--- src/server.js
+++ src/server.js
@@ -2,7 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const userRoutes = require("./routes/userRoutes");
+
+
 const app = express();
+
+
 
 //Middlewares
 app.use(cors());
@@ -11,6 +16,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("CollabFlow Backend server is running");
 });
+
+app.use("/users", userRoutes);
+console.log("User Routes Loaded");
 
 
 const PORT = process.env.PORT || 5000;
