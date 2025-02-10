94937954421b45bf637140d539632274821effb7
ashutos <tripathyashutosh65@gmail.com>
2025-02-10 22:56:08 +0530

    Databse Connection and Synched

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const sequelize = require("./config/database");
+const User = require("./models/User");
 
 const userRoutes = require("./routes/userRoutes");
 
@@ -20,6 +22,28 @@ app.get("/", (req, res) => {
 
 app.use("/user", userRoutes);
 
+sequelize.sync({alter: true,force:false}).then(() => {
+  console.log("Database Synced");
+}).catch((err) => {
+  console.error("Error syncing database", err);
+});
+
+app.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "Name, Email and Password are required" });
+  }
+
+  try {
+    const user = await User.create({ name, email, password: hashedPassword });
+    return res.status(201).json({ message: "User Created Successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Signup Error", error: error.message });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
